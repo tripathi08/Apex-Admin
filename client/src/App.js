1cb9ef8eea4a86b6ee7884c8d51fdbe6ef7bed01
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-11 23:43:50 +0530

    created navbar component using MUI

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,10 @@ import { CssBaseline,ThemeProvider } from "@mui/material";
 import {createTheme}from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard"
 
 
 function App() {
@@ -11,9 +14,17 @@ function App() {
 
   return (
     <div className="app">
+     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline/>
+        <Routes>
+          <Route element={<Layout/>}>
+          <Route path="/" element={<Navigate to="/dashboard" replace/>} />
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          </Route>
+        </Routes>
       </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
