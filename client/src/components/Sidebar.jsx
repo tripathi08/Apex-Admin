32164fd222799be1a67f4992fd3f5ec271711af1
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-13 04:06:01 +0530

    added user data fetched from api calls into navbar and sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -187,7 +187,33 @@ const Sidebar = ({
                 )
              })}
             </List>
-           </Box> 
+           </Box>
+           <Divider/>
+            <Box bottom="2rem" m="0 0 1.25rem 0">
+              <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+                <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{objectFit:"cover"}}
+                />
+                <Box textAlign="left">
+                  <Typography fontWeight="bold" fontSize="0.9rem" sx={{color: theme.palette.secondary[100]}}>
+                    {user.name}
+                  </Typography>
+                  <Typography fontSize="0.8rem" sx={{color: theme.palette.secondary[200]}}>
+                    {user.occupation}
+                  </Typography>
+                </Box>
+                <SettingsOutlined
+                  sx={{color:theme.palette.secondary[300],fontSize:"25px"}}
+                />
+                
+              </FlexBetween>
+              </Box>
           </Drawer>
         )}
     </Box>
