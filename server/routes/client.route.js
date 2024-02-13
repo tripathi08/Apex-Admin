46f30f569b16bc28dc2bf2b34461f83ab917bd9d
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-14 02:50:19 +0530

    created customers page

--- server/routes/client.route.js
+++ server/routes/client.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {getProducts} from "../controllers/client.controller.js"
+import {getProducts,getCustomers} from "../controllers/client.controller.js"
 
 
 const router=express.Router();
 
 router.get("/products",getProducts);
+router.get("/customers",getCustomers);
+
 
 export default router;
