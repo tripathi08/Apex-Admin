6cd6154d44375789b77619c4cd8e617fa222eeb5
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-14 23:12:19 +0530

    created overview page and added line chart

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,7 @@ import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const api=createApi({
     baseQuery: fetchBaseQuery({baseUrl:process.env.REACT_APP_BASE_URL}),
     reducerPath: "adminApi",
-    tagTypes:["User","Products","Customers","Transactions","Geography"],
+    tagTypes:["User","Products","Customers","Transactions","Geography","Sales"],
     endpoints:(build)=>({
         getUser:build.query({
             query:(id)=> `general/user/${id}`,
@@ -28,8 +28,12 @@ export const api=createApi({
         getGeography:build.query({
             query:()=>"client/geography",
             providesTags:["Geography"]
+        }),
+        getSales:build.query({
+            query:()=>"sales/sales",
+            providesTags:["Sales"]
         })
     })
 })
 
-export const {useGetUserQuery,useGetProductsQuery,useGetCustomersQuery,useGetTransactionsQuery,useGetGeographyQuery }=api;
+export const {useGetUserQuery,useGetProductsQuery,useGetCustomersQuery,useGetTransactionsQuery,useGetGeographyQuery,useGetSalesQuery }=api;
--- server/index.js
+++ server/index.js
@@ -10,12 +10,14 @@ import generalRoutes from "./routes/general.route.js";
 import managementRoutes from "./routes/management.route.js";
 import salesRoutes from "./routes/sales.route.js";
 
+
 //data imports
 import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
-import { dataUser,dataProduct,dataProductStat,dataTransaction } from "./data/index.js";
+import OverallStat from "./models/OverallStat.js";
+import { dataUser,dataProduct,dataProductStat,dataTransaction,dataOverallStat } from "./data/index.js";
 
 
 /* CONFIGURATION */
@@ -46,4 +48,5 @@ mongoose
     //Product.insertMany(dataProduct);
     //ProductStat.insertMany(dataProductStat);
     //Transaction.insertMany(dataTransaction);
+    //OverallStat.insertMany(dataOverallStat);
 }).catch((error)=>console.log(`${error} did not connect`))
